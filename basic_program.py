b661fd12e5face1afad3adb800653be13338d125
ANISH-KUMAR-maker <82078372+ANISH-KUMAR-maker@users.noreply.github.com>
2021-04-07 11:22:39 +0530

    Add files via upload

---
+++ basic_program.py
@@ -0,0 +1,15 @@
+def up_low(s):
+    u=l=0
+    for i in list(s):
+        if i. isupper():
+            u=u+1
+        if i. islower():
+            l=l+1
+        else:
+            pass
+    print('no. of upper case characters:', u)
+    print('no. of lower case characters:', l)
+        
+        
+up_low('Hello Mr. Rogers, how are you this fine Tuesday?')
+        
